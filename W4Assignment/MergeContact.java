4c94cc90640800629d6c403652201f07257689c1
Bouvanesswary <88605685+bouvanesswary@users.noreply.github.com>
2023-09-12 19:45:48 -0400

    MergeContact.java

--- W4Assignment/MergeContact.java
+++ W4Assignment/MergeContact.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.ArrayList;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -45,27 +46,46 @@ public class MergeContact {
 		driver.findElement(By.partialLinkText("Merge Contacts")).click();
 		
 		
-		driver.findElement(By.xpath("//img[@alt='Lookup']")).click();
-		Set<String> windowHandles = driver.getWindowHandles();
-		//to switch to a particular index
-		List<String> handles = new ArrayList<>(windowHandles);
-		//switch to the particular window using index
-		driver.switchTo().window(handles.get(1));
+			driver.findElement(By.xpath("//img[@alt='Lookup']")).click();
+			Set<String> windowHandles = driver.getWindowHandles();
+			//to switch to a particular index
+			List<String> handles = new ArrayList<>(windowHandles);
+			//switch to the particular window using index
+			int size = handles.size();
+			System.out.println(size);
+			driver.switchTo().window(handles.get(1));
+			Thread.sleep(3000);
+			System.out.println(driver.getTitle());
+			driver.findElement(By.xpath("//a[@class='linktext']")).click();
 		
+			driver.switchTo().window(handles.get(0));
+			Thread.sleep(3000);
+			System.out.println(driver.getTitle());
+			
+			driver.findElement(By.xpath("(//img[@alt='Lookup'])[2]")).click();
+			//driver.findElement(By.xpath("//a[@id=\"ext-gen628\"]/img")).click();
 		
-		
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//a[@id=\"ext-gen280\"]")).click();
-		//driver.findElement(By.xpath("//div[@id=\"ext-gen293\"]/a")).click();
-		
-		//driver.findElement(By.xpath("//img[@id=\"ext-gen616\"]")).click();
-		//driver.findElement(By.xpath("//a[@id=\"ext-gen295\"]")).click();
-		//driver.findElement(By.className("buttonDangerous")).click();
-		
-		
-		
-		
-		
+			Set<String> windowHandles1 = driver.getWindowHandles();
+			//to switch to a particular index
+			List<String> handles1 = new ArrayList<>(windowHandles1);
+			driver.switchTo().window(handles1.get(1));
+			System.out.println(driver.getTitle());
+			Thread.sleep(3000);
+			
+			driver.findElement(By.xpath("//a[@class='linktext']")).click();
+			driver.switchTo().window(handles.get(0));
+			System.out.println(driver.getTitle());
+			
+			
+			//driver.findElement(By.xpath("//a[@id=\"ext-gen295\"]")).click();
+			driver.findElement(By.className("buttonDangerous")).click();
+			Alert alert = driver.switchTo().alert();
+			System.out.println("The alert is : " +alert.getText()+" Accepted ");
+			alert.accept();
+			System.out.println(driver.getTitle());
+			
+			driver.close();
+				
 	}
 
 }
