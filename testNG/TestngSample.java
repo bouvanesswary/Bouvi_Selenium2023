13a316ef3d286f7122141bc4034afaa8b02ffefd
Bouvanesswary <88605685+bouvanesswary@users.noreply.github.com>
2023-09-17 09:22:18 -0400

    Add files via upload

---
+++ testNG/TestngSample.java
@@ -0,0 +1,30 @@
+package testNG;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestngSample {
+	public  ChromeDriver	driver;
+	@Test
+	 public void loginPage() {
+		 
+		 	 driver = new ChromeDriver();
+			 driver.manage().window().maximize();
+			 driver.get("http://leaftaps.com/opentaps/control/main");
+			 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+			 System.out.println("Sucess");
+			 driver.close();
+	       }  
+	
+	public static void main(String[] args) 
+	{
+		TestngSample ts=new TestngSample();
+		ts.loginPage();
+		
+	}
+	}
+	
+	
